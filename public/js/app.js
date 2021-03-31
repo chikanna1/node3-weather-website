ff1760ff4c6f4f1732ccb78e5abb59b1b46c7f79
Chika Nna <chika.ekene@gmail.com>
2021-03-31 17:21:59 +0100

    Setup App for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,20 +11,18 @@ weatherForm.addEventListener("submit", (e) => {
 
   const location = search.value;
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-          messageTwo.textContent = "";
-          console.log(data.error);
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-          console.log(data.location);
-          console.log(data.forecast);
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = "";
+        console.log(data.error);
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const { response } = require("express");
 console.log(__dirname);
 console.log(path.join(__dirname, "../public"));
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define Paths for Express Config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -97,6 +98,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on Port 3000");
+app.listen(port, () => {
+  console.log("Server is up on Port " + port);
 });
